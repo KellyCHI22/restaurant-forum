7914375fdd9a2b1a45c97c656c3a8db20d7a133f
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-13 15:29:08 +0200

    feat: add user model

--- models/index.js
+++ models/index.js
@@ -13,17 +13,26 @@ let sequelize
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  )
 }
 
 // 動態引入其他 models
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    )
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    )
     db[model.name] = model
   })
 
@@ -35,6 +44,7 @@ Object.keys(db).forEach(modelName => {
 })
 
 // 匯出需要的物件
+// * 前者是 instance，後者是 Class
 db.sequelize = sequelize
 db.Sequelize = Sequelize
 

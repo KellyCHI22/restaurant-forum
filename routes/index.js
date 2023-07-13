f2bbfd304d4b351cb03b583383363015d8bed98f
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-13 15:48:04 +0200

    feat: add user signup

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const restaurantController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) // 注意用 post
 
 router.use('/admin', admin)
-
 router.get('/restaurants', restaurantController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))

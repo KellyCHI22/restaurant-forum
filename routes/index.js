094d6d80095bbbe9124eee4b549546b40beda8d5
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-15 16:05:06 +0200

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('../config/passport')
-const admin = require('./modules/admin')
-const restaurantController = require('../controllers/restaurant-controller')
-const userController = require('../controllers/user-controller')
-const { authenticated } = require('../middleware/auth')
-const { generalErrorHandler } = require('../middleware/error-handler')
+const express = require('express');
+const router = express.Router();
+const passport = require('../config/passport');
+const admin = require('./modules/admin');
+const restaurantController = require('../controllers/restaurant-controller');
+const userController = require('../controllers/user-controller');
+const { authenticated, authenticatedAdmin } = require('../middleware/auth');
+const { generalErrorHandler } = require('../middleware/error-handler');
 
-router.use('/admin', admin)
-router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp) // 注意用 post
+router.use('/admin', authenticatedAdmin, admin);
+router.get('/signup', userController.signUpPage);
+router.post('/signup', userController.signUp); // 注意用 post
 
-router.get('/signin', userController.signInPage)
+router.get('/signin', userController.signInPage);
 router.post(
   '/signin',
   passport.authenticate('local', {
     failureRedirect: '/signin',
-    failureFlash: true
+    failureFlash: true,
   }),
   userController.signIn
-) // 注意是 post
-router.get('/logout', userController.logout)
+); // 注意是 post
+router.get('/logout', userController.logout);
 
-router.get('/restaurants', authenticated, restaurantController.getRestaurants)
+router.get('/restaurants', authenticated, restaurantController.getRestaurants);
 
-router.use('/', (req, res) => res.redirect('/restaurants'))
-router.use('/', generalErrorHandler) // error handler 有特出處理方式，因此順序無關
+router.use('/', (req, res) => res.redirect('/restaurants'));
+router.use('/', generalErrorHandler); // error handler 有特出處理方式，因此順序無關
 
-module.exports = router
+module.exports = router;

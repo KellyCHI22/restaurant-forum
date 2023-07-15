2edc6227ee1e82e5f6b8885f8d467e623811ac64
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-15 17:47:13 +0200

    feat: delete restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const adminController = require('../../controllers/admin-controller')
+const express = require('express');
+const router = express.Router();
+const adminController = require('../../controllers/admin-controller');
 
 // * 特別留意路由擺放的順序，應從嚴格到寬鬆
-router.get('/restaurants/create', adminController.createRestaurant)
-router.get('/restaurants/:id/edit', adminController.editRestaurant)
-router.get('/restaurants/:id', adminController.getRestaurant) // 需擺在 /create 後面
-router.put('/restaurants/:id', adminController.putRestaurant)
-router.get('/restaurants', adminController.getRestaurants)
-router.post('/restaurants', adminController.postRestaurant)
-router.use('/', (req, res) => res.redirect('/admin/restaurants'))
+router.get('/restaurants/create', adminController.createRestaurant);
+router.get('/restaurants/:id/edit', adminController.editRestaurant);
+router.get('/restaurants/:id', adminController.getRestaurant); // 需擺在 /create 後面
+router.put('/restaurants/:id', adminController.putRestaurant);
+router.delete('/restaurants/:id', adminController.deleteRestaurant);
+router.get('/restaurants', adminController.getRestaurants);
+router.post('/restaurants', adminController.postRestaurant);
+router.use('/', (req, res) => res.redirect('/admin/restaurants'));
 
-module.exports = router
+module.exports = router;

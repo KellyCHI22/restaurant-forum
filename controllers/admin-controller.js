74f8b0d9f352610e5a2874865055aa9de90a591e
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-15 16:31:06 +0200

    feat: admin restaurant page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,34 +1,46 @@
-const { Restaurant } = require('../models');
+const { Restaurant } = require('../models')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
-      raw: true, //  把 Sequelize 包裝過的一大包物件轉換成格式比較單純的 JS 原生物件
+      raw: true //  把 Sequelize 包裝過的一大包物件轉換成格式比較單純的 JS 原生物件
     })
-      .then((restaurants) => res.render('admin/restaurants', { restaurants }))
-      .catch((err) => next(err));
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   },
   createRestaurant: (req, res) => {
-    return res.render('admin/create-restaurant');
+    return res.render('admin/create-restaurant')
   },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body; // 從 req.body 拿出表單裡的資料
+    const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
     // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
     // ! 後端需做驗證，防止有心人士調整前端程式碼
-    if (!name) throw new Error('Restaurant name is required!');
+    if (!name) throw new Error('Restaurant name is required!')
     Restaurant.create({
       // 產生一個新的 Restaurant 物件實例，並存入資料庫
       name,
       tel,
       address,
       openingHours,
-      description,
+      description
     })
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully created'); // 在畫面顯示成功提示
-        res.redirect('/admin/restaurants'); // 新增完成後導回後台首頁
+        req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
+        res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
       })
-      .catch((err) => next(err));
+      .catch(err => next(err))
   },
-};
-module.exports = adminController;
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      // 去資料庫用 id 找一筆資料
+      raw: true // 找到以後整理格式再回傳
+    })
+      .then(restaurant => {
+        //  如果找不到，回傳錯誤訊息，後面不執行
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../../controllers/admin-controller');
+const express = require('express')
+const router = express.Router()
+const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants/create', adminController.createRestaurant);
-router.get('/restaurants', adminController.getRestaurants);
-router.post('/restaurants', adminController.postRestaurant);
-router.use('/', (req, res) => res.redirect('/admin/restaurants'));
+// * 特別留意路由擺放的順序，應從嚴格到寬鬆
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant) // 需擺在 /create 後面
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
+router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
-module.exports = router;
+module.exports = router

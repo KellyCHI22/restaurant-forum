845eab8dd015f521bff79fe8aaf4045e982c69a3
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-19 15:21:20 +0200

    feat: add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -11,11 +11,18 @@ const adminController = {
       .then((restaurants) => res.render('admin/restaurants', { restaurants }))
       .catch((err) => next(err));
   },
-  createRestaurant: (req, res) => {
-    return res.render('admin/create-restaurant');
+  createRestaurant: (req, res, next) => {
+    return Category.findAll({
+      raw: true,
+    })
+      .then((categories) =>
+        res.render('admin/create-restaurant', { categories })
+      )
+      .catch((err) => next(err));
   },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body; // 從 req.body 拿出表單裡的資料
+    const { name, tel, address, openingHours, description, categoryId } =
+      req.body; // 從 req.body 拿出表單裡的資料
     // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
     // ! 後端需做驗證，防止有心人士調整前端程式碼
     if (!name) throw new Error('Restaurant name is required!');
@@ -30,6 +37,7 @@ const adminController = {
           openingHours,
           description,
           image: filePath || null,
+          categoryId,
         })
       )
       .then(() => {
@@ -53,17 +61,20 @@ const adminController = {
       .catch((err) => next(err));
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
-      raw: true,
-    })
-      .then((restaurant) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!");
-        res.render('admin/edit-restaurant', { restaurant });
+    // 使用 Promise.all 增加程式處理效率
+    return Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true }),
+    ])
+      .then(([restaurant, categories]) => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!");
+        res.render('admin/edit-restaurant', { restaurant, categories });
       })
       .catch((err) => next(err));
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body;
+    const { name, tel, address, openingHours, description, categoryId } =
+      req.body;
     if (!name) throw new Error('Restaurant name is required!');
     const { file } = req;
     Promise.all([
@@ -82,6 +93,7 @@ const adminController = {
           image: filePath || restaurant.image,
           // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath
           // 是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
+          categoryId,
         });
       })
       .then(() => {

a9f743e76229a854d723d108689a66fba4b22ea6
Hsin-tingCHI <chihsinting@gmail.com>
2023-08-15 12:14:57 +0200

    refactor: extract category-service

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,11 +1,10 @@
-const { Category } = require('../models')
+const categoryService = require('../services/category-service')
+
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Promise.all([
-      Category.findAll({ raw: true }),
-      // 檢查 req.params.id 這個變數是否存在，若存在則額外取出該 category 資料
-      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
-    ])
+    const categoryId = req.params.id
+    return categoryService
+      .getCategories(categoryId)
       .then(([categories, category]) => {
         res.render('admin/categories', {
           categories,
@@ -17,9 +16,8 @@ const categoryController = {
   postCategory: (req, res, next) => {
     const { name } = req.body
     if (!name) throw new Error('Category name is required!')
-    Category.create({
-      name
-    })
+    return categoryService
+      .postCategory(name, next)
       .then(() => {
         req.flash('success_messages', 'Category was successfully created')
         res.redirect('/admin/categories')
@@ -29,11 +27,8 @@ const categoryController = {
   putCategory: (req, res, next) => {
     const { name } = req.body
     if (!name) throw new Error('Category name is required!')
-    return Category.findByPk(req.params.id)
-      .then(category => {
-        if (!category) throw new Error("Category doesn't exist!")
-        return category.update({ name })
-      })
+    return categoryService
+      .putCategory(req.params.id, name, next)
       .then(() => {
         req.flash('success_messages', 'Category was successfully updated')
         res.redirect('/admin/categories')
@@ -41,12 +36,12 @@ const categoryController = {
       .catch(err => next(err))
   },
   deleteCategory: (req, res, next) => {
-    return Category.findByPk(req.params.id)
-      .then(category => {
-        if (!category) throw new Error("Category didn't exist!")
-        return category.destroy()
+    return categoryService
+      .deleteCategory(req.params.id)
+      .then(() => {
+        req.flash('success_messages', 'Category was successfully deleted')
+        res.redirect('/admin/categories')
       })
-      .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   }
 }

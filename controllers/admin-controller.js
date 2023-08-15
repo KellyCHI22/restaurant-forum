74f7bf7f9d2542e60b16f1f3b382c07fb79ebf1b
Hsin-tingCHI <chihsinting@gmail.com>
2023-08-15 17:28:02 +0200

    refactor: extract admin-service

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,5 @@
-const { Restaurant, User, Category } = require('../models')
-const { localFileHandler } = require('../helpers/file-helpers')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const adminService = require('../services/admin-service')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -9,13 +8,8 @@ const adminController = {
     const limit = Number(req.query.limit) || DEFAULT_LIMIT
     const offset = getOffset(limit, page)
 
-    return Restaurant.findAndCountAll({
-      raw: true, //  把 Sequelize 包裝過的一大包物件轉換成格式比較單純的 JS 原生物件
-      nest: true, // 把 category 資料包裝成更容易使用的形式
-      include: [Category], // 使用 model 的關聯資料時，需要透過 include 把關聯資料拉進來
-      limit,
-      offset
-    })
+    return adminService
+      .getRestaurants(limit, offset, next)
       .then(restaurants =>
         res.render('admin/restaurants', {
           restaurants: restaurants.rows,
@@ -25,9 +19,8 @@ const adminController = {
       .catch(err => next(err))
   },
   createRestaurant: (req, res, next) => {
-    return Category.findAll({
-      raw: true
-    })
+    return adminService
+      .createRestaurant(next)
       .then(categories =>
         res.render('admin/create-restaurant', { categories })
       )
@@ -40,47 +33,38 @@ const adminController = {
     // ! 後端需做驗證，防止有心人士調整前端程式碼
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
-    localFileHandler(file)
-      .then(filePath =>
-        Restaurant.create({
-          // 產生一個新的 Restaurant 物件實例，並存入資料庫
+    return adminService
+      .postRestaurant(
+        {
           name,
           tel,
           address,
           openingHours,
           description,
-          image: filePath || null,
-          categoryId
-        })
+          categoryId,
+          file
+        },
+        next
       )
-      .then(() => {
+      .then(restaurant => {
+        console.log(restaurant.toJSON())
         req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
         res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
       })
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
-      // 去資料庫用 id 找一筆資料
-      raw: true, // 找到以後整理格式再回傳
-      nest: true,
-      include: [Category]
-    })
+    return adminService
+      .getRestaurant(req.params.id, next)
       .then(restaurant => {
-        //  如果找不到，回傳錯誤訊息，後面不執行
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('admin/restaurant', { restaurant })
       })
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    // 使用 Promise.all 增加程式處理效率
-    return Promise.all([
-      Restaurant.findByPk(req.params.id, { raw: true }),
-      Category.findAll({ raw: true })
-    ])
+    return adminService
+      .editRestaurant(req.params.id, next)
       .then(([restaurant, categories]) => {
-        if (!restaurant) throw new Error("Restaurant doesn't exist!")
         res.render('admin/edit-restaurant', { restaurant, categories })
       })
       .catch(err => next(err))
@@ -90,56 +74,44 @@ const adminController = {
       req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
-    Promise.all([
-      // 非同步處理
-      Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
-      localFileHandler(file) // 把檔案傳到 file-helper 處理
-    ])
-      .then(([restaurant, filePath]) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return restaurant.update({
+    return adminService
+      .putRestaurant(
+        req.params.id,
+        {
           name,
           tel,
           address,
           openingHours,
           description,
-          image: filePath || restaurant.image,
-          // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath
-          // 是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
-          categoryId
-        })
-      })
+          categoryId,
+          file
+        },
+        next
+      )
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully to update')
+        req.flash('success_messages', 'Restaurant was successfully updated')
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
   },
   deleteRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return restaurant.destroy()
+    return adminService
+      .deleteRestaurant(req.params.id, next)
+      .then(() => {
+        req.flash('success_messages', 'Restaurant was successfully deleted')
+        res.redirect('/admin/restaurants')
       })
-      .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({
-      raw: true
-    })
+    return adminService
+      .getUsers(next)
       .then(users => res.render('admin/users', { users }))
       .catch(err => next(err))
   },
   patchUsers: (req, res, next) => {
-    return User.findByPk(req.params.id)
-      .then(user => {
-        if (!user) throw new Error('User does not exist!')
-        if (user.name === 'root') throw new Error('禁止變更 root 權限！')
-        return user.update({
-          isAdmin: !user.isAdmin
-        })
-      })
+    return adminService
+      .patchUsers(req.params.id, next)
       .then(() => {
         req.flash('success_messages', '使用者權限變更成功')
         res.redirect('/admin/users')
---
+++ services/admin-service.js
@@ -0,0 +1,111 @@
+const { Restaurant, User, Category } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers')
+
+const adminService = {
+  getRestaurants: (limit, offset, next) => {
+    return Restaurant.findAndCountAll({
+      raw: true, //  把 Sequelize 包裝過的一大包物件轉換成格式比較單純的 JS 原生物件
+      nest: true, // 把 category 資料包裝成更容易使用的形式
+      include: [Category], // 使用 model 的關聯資料時，需要透過 include 把關聯資料拉進來
+      limit,
+      offset
+    }).catch(err => next(err))
+  },
+  createRestaurant: next => {
+    return Category.findAll({
+      raw: true
+    }).catch(err => next(err))
+  },
+  postRestaurant: (restaurantData, next) => {
+    const { name, tel, address, openingHours, description, categoryId, file } =
+      restaurantData
+    return localFileHandler(file)
+      .then(filePath => {
+        return Restaurant.create({
+          // 產生一個新的 Restaurant 物件實例，並存入資料庫
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || null,
+          categoryId,
+          viewCounts: 0
+        })
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (restaurantId, next) => {
+    return Restaurant.findByPk(restaurantId, {
+      // 去資料庫用 id 找一筆資料
+      raw: true, // 找到以後整理格式再回傳
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        //  如果找不到，回傳錯誤訊息，後面不執行
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant
+      })
+      .catch(err => next(err))
+  },
+  editRestaurant: (restaurantId, next) => {
+    return Promise.all([
+      Restaurant.findByPk(restaurantId, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!")
+        return [restaurant, categories]
+      })
+      .catch(err => next(err))
+  },
+  putRestaurant: (restaurantId, restaurantData, next) => {
+    const { name, tel, address, openingHours, description, categoryId, file } =
+      restaurantData
+    return Promise.all([
+      Restaurant.findByPk(restaurantId), // 去資料庫查有沒有這間餐廳
+      localFileHandler(file) // 把檔案傳到 file-helper 處理
+    ])
+      .then(([restaurant, filePath]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.update({
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || restaurant.image,
+          // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath
+          // 是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
+          categoryId
+        })
+      })
+      .catch(err => next(err))
+  },
+  deleteRestaurant: (restaurantId, next) => {
+    return Restaurant.findByPk(restaurantId)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .catch(err => next(err))
+  },
+  getUsers: next => {
+    return User.findAll({
+      raw: true
+    }).catch(err => next(err))
+  },
+  patchUsers: (userId, next) => {
+    return User.findByPk(userId)
+      .then(user => {
+        if (!user) throw new Error('User does not exist!')
+        if (user.name === 'root') throw new Error('禁止變更 root 權限！')
+        return user.update({
+          isAdmin: !user.isAdmin
+        })
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = adminService
